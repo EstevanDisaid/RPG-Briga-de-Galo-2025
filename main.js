bb81414e3493ff18d9a15eba75003b9b61b73951
Estevan Neri Disaid Emerenciano <141413662+EstevanDisaid@users.noreply.github.com>
2025-06-26 11:07:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,44 +8,90 @@ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "alternativa 01",
+                afirmacao: "resultado 01"
+            },
+
+            {
+                texto: "alternativa 02",
+                afirmacao: "resultado 02"
+            }
         ]
     },
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativa 3",
-            "Alterantiva 4"
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado 03"
+            },
+
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+            }
         ]
     },
- 
+
     {
         enunciado: "Pergunta 3",
         alternativas: [
-            "Alternativa 5",
-            "Alterantiva 6"
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            }
         ]
     },
 
     {
         enunciado: "Pergunta 4",
         alternativas: [
-            "Alternativa 7",
-            "Alterantiva 8"
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
         ]
     }
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
-perguntaAtual = perguntas[atual]
-caixaPerguntas.textContent = perguntaAtual.enunciado
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+    mostraAlternativas()
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button")
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    atual++
+    mostraPergunta();
 }
 
 
 mostraPergunta();
 
 console.log(perguntas)
+
+
